c333da428da0e00dd06b28330a81d4b07721e46a
fendy <chanes3222@gmail.com>
2024-08-25 12:38:27 +0800

    動畫調整，字體大小調整

--- assets/js/aos.js
+++ assets/js/aos.js
@@ -2,19 +2,45 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 AOS.init();
 
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'center-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const animatedHeading = document.getElementById('animate__bounce');
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                animatedHeading.classList.add('animate__animated', 'animate__bounce');
+                animatedHeading.classList.add('animate__animated', 'animate__bounce',);
             } else {
                 animatedHeading.classList.remove('animate__animated', 'animate__bounce');
             }
         });
     }, {
-        threshold: 0.1 // 當元素進入視窗 10% 時觸發
+        threshold: 0.1
     });
 
     observer.observe(animatedHeading);
@@ -32,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }, {
-        threshold: 0.1 // 當元素進入視窗 10% 時觸發
+        threshold: 0.1
     });
 
     observer.observe(animatedHeading);
@@ -50,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }, {
-        threshold: 0.1 // 當元素進入視窗 10% 時觸發
+        threshold: 0.1
     });
 
     observer.observe(animatedHeading);
